74c90299215033b240b8aab181f85f9ee4ec396c
Kotov-Roman <poma_kotov@mail.ru>
2018-11-22 08:35:23 +0400

    working version with javadocks , but without junit tests

--- src/main/java/algo/BinarySearch.java
+++ src/main/java/algo/BinarySearch.java
@@ -10,9 +10,9 @@ import java.util.regex.Pattern;
  * A Binary Search realization class.
  * It finds an element position in an already sorted array and returns its position number.
  * If no such element found, it returns -1.
- *
+ * <p>
  * {@code digitsPattern} is a pattern for checking number to find input. If input does not match this pattern,
- *                      it throws IllegalArgumentException.
+ * it throws IllegalArgumentException.
  * {@throws} IllegalArgumentException.
  */
 public class BinarySearch {
@@ -23,7 +23,7 @@ public class BinarySearch {
         int lowerBound = 0;
         int highBound = targetArray.length - 1;
 
-        while(lowerBound <= highBound) {
+        while (lowerBound <= highBound) {
             int middleBound = lowerBound + (highBound - lowerBound) / 2;
             if (elementToFind < targetArray[middleBound]) {
                 highBound = middleBound - 1;
@@ -37,21 +37,24 @@ public class BinarySearch {
     }
 
     private int enterElementToFind() {
-        System.out.println("Enter a number in array to find its position: ");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String element = "";
-        try {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Enter a number in array to find its position: ");
+            String element = "";
             element = reader.readLine();
+
+
+            int elementToFind;
+            Matcher digitsMatcher = digitsPattern.matcher(element);
+            if (!digitsMatcher.matches()) {
+                throw new IllegalArgumentException(element + " is not a number");
+            } else {
+                elementToFind = Integer.parseInt(element);
+            }
+            return elementToFind;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        int elementToFind;
-        Matcher digitsMatcher = digitsPattern.matcher(element);
-        if (!digitsMatcher.matches()) {
-            throw new IllegalArgumentException(element + " is not a number");
-        } else {
-            elementToFind = Integer.parseInt(element);
-        }
-        return elementToFind;
+        return -10;
     }
 }
